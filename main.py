e2bbdb284be80a429ee55d805c099a971c13dd18
Yifan Yang <yifanyeung@sjtu.edu.cn>
2025-01-26 04:28:14 +0000

    feat: vad

--- main.py
+++ main.py
@@ -16,10 +16,13 @@ import torchaudio
 
 from download import install_requirements
 
+from silero_vad import load_silero_vad, get_speech_timestamps
+
 model_size = 30000
 mimi = None
 lm_gen = None
 device = None
+vad_model = None
 
 
 async def inference(request: Request) -> StreamingResponse:
@@ -38,14 +41,20 @@ async def inference(request: Request) -> StreamingResponse:
     audio_data = data["audio_data"]
 
     target_sample_rate = 24000
+    vad_final_sample_rate = 16000
     frame_size = int(mimi.sample_rate / mimi.frame_rate)
     wav = torch.tensor(audio_data).unsqueeze(0)
     wav = torchaudio.transforms.Resample(sample_rate, target_sample_rate)(wav)
     # pad to multiple of frame_size
     wav = torch.nn.functional.pad(wav, (0, frame_size - wav.size(-1) % frame_size))
-    # # pad 10 seconds white
-    # emtpy_frames = torch.zeros(1, 1920 * 10)
-    # wav = torch.cat([emtpy_frames, wav], dim=-1)
+    # pad pad_secs_pre seconds white before the audio, and pad_secs_post seconds white after the audio
+    pad_secs_pre = 5
+    wav_secs = wav.size(-1) / target_sample_rate
+    pad_secs_post = int(235 - wav_secs)
+    emtpy_frames_pre = torch.zeros(1, 1920 * pad_secs_pre)
+    emtpy_frames_post = torch.zeros(1, 1920 * pad_secs_post)
+    wav = torch.cat([emtpy_frames_pre, wav], dim=-1)
+    wav = torch.cat([wav, emtpy_frames_post], dim=-1)
     wav = wav.unsqueeze(0).to(device)
 
     with torch.no_grad():
@@ -74,14 +83,32 @@ async def inference(request: Request) -> StreamingResponse:
 
     # print("Output text:", "".join(text_chunks))
     result_arr = decoded.squeeze(0).cpu()
+    # cut pad_secs_pre off from result_arr
+    result_arr = result_arr[:, int((pad_secs_pre - 1) * target_sample_rate) :]
+    result_arr = torchaudio.transforms.Resample(
+        target_sample_rate, vad_final_sample_rate
+    )(result_arr)
+    result_arr = result_arr.squeeze(0)
+
+    try:
+        speech_timestamps = get_speech_timestamps(
+            result_arr, vad_model, sampling_rate=vad_final_sample_rate
+        )
+        result_arr = result_arr[
+            int(speech_timestamps[0]["start"]) : int(speech_timestamps[-1]["end"])
+        ]
+    except Exception as e:
+        print(e)
+    result_arr = result_arr.unsqueeze(0)
+
     result = io.BytesIO()
-    torchaudio.save(result, result_arr, target_sample_rate, format="wav")
+    torchaudio.save(result, result_arr, vad_final_sample_rate, format="wav")
     result.seek(0)
     return StreamingResponse(result, media_type="application/octet-stream")
 
 
 def init():
-    global mimi, device, lm_gen
+    global mimi, device, lm_gen, vad_model
     install_requirements()
     device = "cuda"
     mimi_weight = hf_hub_download(loaders.DEFAULT_REPO, loaders.MIMI_NAME)
@@ -90,12 +117,14 @@ def init():
     moshi_weight = hf_hub_download(loaders.DEFAULT_REPO, loaders.MOSHI_NAME)
     moshi = loaders.get_moshi_lm(moshi_weight, device=device)
     lm_gen = LMGen(moshi, temp=0.8, temp_text=0.7)
+    vad_model = load_silero_vad()
 
 
 def hangup():
-    global mimi, lm_gen
+    global mimi, lm_gen, vad_model
     del mimi
     del lm_gen
+    del vad_model
     torch.cuda.empty_cache()
 
 
